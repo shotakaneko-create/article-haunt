93f6a8675d3566368e9623b0a72984af773d0988
菖汰 金子 <kk.714b.st@icloud.com>
2021-09-20 18:01:05 +0900

    article の CRUD処理を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # mount_devise_token_auth_for "User", at: "auth"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    namespace :api do
-      namespace :v1 do
-        mount_devise_token_auth_for "User", at: "auth"
-        resources :articles
-      end
+
+  # namespace :●● do ルーティングの定義に名前空間を付ける
+  # この場合だと app/controllers/api/v1/articles_controller.rb の
+  # Api::V1::ArticlesControllerが呼び出される
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth"
+      resources :articles
     end
   end
 end
