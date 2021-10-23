84bd3046f665f18da2288c8d7f279575611965ab
菖汰 金子 <kk.714b.st@icloud.com>
2021-10-23 23:27:40 +0900

    enum のテストを追加

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
-  attributes :id, :title, :updated_at # attributes:このクラスは何をパラメータとして受け取るのかを明示→出力する値を設定
+  attributes :id, :title, :updated_at, :status # attributes:このクラスは何をパラメータとして受け取るのかを明示→出力する値を設定
 
   # has_many :likes
   # has_many :comments
--- app/serializers/api/v1/article_serializer.rb
+++ app/serializers/api/v1/article_serializer.rb
@@ -1,4 +1,4 @@
 class Api::V1::ArticleSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :updated_at # attributes:このクラスは何をパラメータとして受け取るのかを明示→出力する値を設定
+  attributes :id, :title, :body, :updated_at, :status # attributes:このクラスは何をパラメータとして受け取るのかを明示→出力する値を設定
   belongs_to :user, serializer: Api::V1::UserSerializer
 end
